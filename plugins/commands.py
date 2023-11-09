9eee2b4a24bf4a2d61d3b39279852ecb59d6f25e
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-09 20:36:15 +0500

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,25 +21,16 @@ from utils import extract_link, get_me_button, get_size
 logger = logging.getLogger(__name__)
 
 user_commands = [
-    "mdisk_api",
     "shortener_api",
     "header",
     "footer",
     "username",
     "banner_image",
-    "base_site",
     "me",
 ]
 avl_web = [
-    "droplink.co",
-    "gplinks.in",
-    "tnlink.in",
-    "za.gl",
-    "du-link.in",
-    "viplink.in",
-    "shorturllink.in",
-    "shareus.in",
-    "earnspace.in",
+    "zxlink.in",
+    "krishnalink.com",
 ]
 
 avl_web1 = "".join(f"- {i}\n" for i in avl_web)
