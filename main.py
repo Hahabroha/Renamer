587ca2112e44ffa0cbee2268187aef07ae313eef
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-11 13:19:44 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ from telegram.ext import Updater, CommandHandler
 # Load environment variables from .env file
 load_dotenv()
 
-# Replace "YOUR_BOT_TOKEN" with your actual Telegram bot token
+# Replace "YOUR_ACTUAL_BOT_TOKEN" with your real bot token
 bot_token = os.environ.get("6563388880:AAEePBpgrmEOsEC2D3VB8QnF3ADiEIK87Hk")
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.")
 
+def verify(update, context):
+    user = update.effective_user
+    original_link = context.args[0] if context.args else None
+
+    # Your verification logic here
+
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Verification logic executed. Original link: {original_link}",
+    )
+
 def main() -> None:
-    updater = Updater("6563388880:AAEePBpgrmEOsEC2D3VB8QnF3ADiEIK87Hk")
+    updater = Updater(token=bot_token)
     dp = updater.dispatcher
 
     # Register command handlers
@@ -21,7 +32,6 @@ def main() -> None:
 
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
