90b454f4aae3d3ce10928278a10998cc2d50d691
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-11 17:42:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,8 @@
 import re
-from os import environ,getenv 
+from os import environ,getenv
+from Script import script 
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
+id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
