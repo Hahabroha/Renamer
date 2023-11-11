662542802a7e704a15c2eb05ac31008b855932af
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-11 13:02:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from bot import Bot
+from telegram.ext import Updater, CommandHandler
+import os
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler  # Add this line
+
+# Load environment variables from .env file
+load_dotenv()
+
+# ... (rest of your code)
+
 def main() -> None:
-    updater = Updater("6563388880:AAEePBpgrmEOsEC2D3VB8QnF3ADiEIK87Hk")
+    updater = Updater("YOUR_BOT_TOKEN")
     dp = updater.dispatcher
 
     # Register command handlers
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("verify", verify, pass_args=True))  # Add this line
+    dp.add_handler(CommandHandler("verify", verify, pass_args=True))
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     main()
-    
