38fa6338ed5d60a2ff4aa476068687602fe3ddaf
Hahabroha <146943572+Hahabroha@users.noreply.github.com>
2023-11-11 12:58:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from bot import Bot
+def main() -> None:
+    updater = Updater("6563388880:AAEePBpgrmEOsEC2D3VB8QnF3ADiEIK87Hk")
+    dp = updater.dispatcher
+
+    # Register command handlers
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("verify", verify, pass_args=True))  # Add this line
+
+    updater.start_polling()
+    updater.idle()
 
 if __name__ == '__main__':
-    Bot().run()
+    main()
+    
